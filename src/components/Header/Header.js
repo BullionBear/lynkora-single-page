2e39aabbfbd4290176578f0e70c39c5f68cb3812
yite <coastq22889@icloud.com>
2023-12-31 17:31:07 +0800

    use cute header font

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,11 +5,10 @@ function Header() {
     return (
         <header className="header">
             <div className="logo">
-                {/* You can add your logo image or text here */}
-                My App
+                {/* Correct way to reference the image in the public folder */}
+                <img src="/images/logo.png" alt="Logo" />
             </div>
             <nav className="navigation">
-                {/* Add your navigation links here */}
                 <a href="#home">Home</a>
                 <a href="#about">About</a>
                 <a href="#services">Services</a>
