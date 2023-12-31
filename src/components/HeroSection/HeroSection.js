1cbffbff818985c8cea66353079c2e481167e9d4
yite <coastq22889@icloud.com>
2023-12-31 17:59:50 +0800

    copywriting of Lynkora

--- src/components/HeroSection/HeroSection.js
+++ src/components/HeroSection/HeroSection.js
@@ -3,10 +3,18 @@ import './HeroSection.css';
 
 function HeroSection() {
     return (
-        <div id="home" div className="hero-section">
-            <h1>Welcome to My React App</h1>
-            <p>This is a simple hero section to catch your visitor's attention.</p>
-            <button>Learn More</button>
+        <div id="home" className="hero-section">
+            <div className="hero-content">
+                <div className="hero-text">
+                    <p>Where Mathematics Meets Financial Innovation. At Lynkora, we believe in harnessing
+                        the power of advanced mathematical models to revolutionize the way financial 
+                        products are crafted. Our mission is to innovate and optimize, transforming complex 
+                        financial activities into streamlined, efficient solutions.
+                    </p>
+                    <button>Learn More</button>
+                </div>
+                <h1>Welcome to Lynkora</h1>
+            </div>
         </div>
     );
 }
