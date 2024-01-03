f01923c7bee8b0e12c89f656b97af24c46b1d029
yite <yite@orichal.io>
2024-01-03 20:47:10 +0800

    add credential

--- README.md
+++ README.md
@@ -1,29 +1,11 @@
-# Stage 1: Build the React application
-FROM node:20 AS build
+# Lynkora
+## Run in Node.js
+```bash
+npm start
+```
 
-# Set the working directory in the Docker container
-WORKDIR /app
-
-# Copy package.json and package-lock.json (or yarn.lock) to the Docker container
-COPY package*.json /app/
-
-# Install dependencies in the Docker container
-RUN npm install
-
-# Copy the rest of your app's source code from your host to your image filesystem.
-COPY . /app
-
-# Build the application
-RUN npm run build
-
-# Stage 2: Serve the React application from Nginx
-FROM nginx:alpine
-
-# Copy the build output to replace the default nginx contents.
-COPY --from=build /app/build /usr/share/nginx/html
-
-# Expose port 80
-EXPOSE 80
-
-# Start Nginx and keep it running
-CMD ["nginx", "-g", "daemon off;"]
+## Run in docker
+```bash
+make build 
+make run
+```
--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -6,7 +6,9 @@ function Contact() {
     const [email, setEmail] = useState('');
     const [message, setMessage] = useState('');
     const [submitStatus, setSubmitStatus] = useState('');
+    const [isLoading, setIsLoading] = useState(false);
 
+    
     function handleSubmit(event) {
         event.preventDefault(); 
         console.log("Form submitted");
@@ -17,13 +19,22 @@ function Contact() {
             message
         };
 
-        fetch('https://lynkora.com:8000/v1/submit', {
+        // Set loading to true
+        setIsLoading(true);
+
+        // Implementing timeout for fetch
+        const fetchTimeout = new Promise((resolve, reject) => {
+            setTimeout(() => reject(new Error('Request timed out')), 5000); // 5000 ms timeout
+        });
+
+        Promise.race([fetchTimeout, fetch(`${process.env.REACT_APP_API_BASE_URL}/v1/submit`, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
+                'API-Key': `${process.env.REACT_APP_API_KEY}`
             },
             body: JSON.stringify(formData),
-        })
+        })])
         .then(response => {
             if (!response.ok) {
                 throw new Error('Network response was not ok');
@@ -37,6 +48,9 @@ function Contact() {
         .catch((error) => {
             console.error('Error:', error);
             setSubmitStatus('Failed to send message.');
+        })
+        .finally(() => {
+            setIsLoading(false); // Reset loading state
         });
     }
 
@@ -47,8 +61,9 @@ function Contact() {
                 <input type="text" placeholder="Your Name" value={name} onChange={(e) => setName(e.target.value)} />
                 <input type="email" placeholder="Your Email" value={email} onChange={(e) => setEmail(e.target.value)} />
                 <textarea placeholder="Your Message" value={message} onChange={(e) => setMessage(e.target.value)}></textarea>
-                <button type="submit">Send Message</button>
-                {submitStatus && <p className="submit-status">{submitStatus}</p>} {/* Display status message */}
+                <button type="submit" disabled={isLoading}>Send Message</button>
+                {isLoading && <p>Loading...</p>} {/* Show loading message */}
+                {submitStatus && <p className="submit-status">{submitStatus}</p>}
             </form>
         </div>
     );
